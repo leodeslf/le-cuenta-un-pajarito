e33901f9e9086a6c362b3e34217ad88f6ffed582
Leo <leodesouzauy@hotmail.com>
2019-09-10 18:52:42 -0300

    Updates

---
+++ precache-manifest.40f5897dc87e719e5e32262a6ddd9f02.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff85f63451e6f1fa9d118ab65b9c69b0",
+    "url": "/le-cuenta-un-pajarito/index.html"
+  },
+  {
+    "revision": "e663b88e49f129cf8be9",
+    "url": "/le-cuenta-un-pajarito/static/css/main.615b3c03.chunk.css"
+  },
+  {
+    "revision": "d35d344bc543654f2b0d",
+    "url": "/le-cuenta-un-pajarito/static/js/2.9e897f10.chunk.js"
+  },
+  {
+    "revision": "e663b88e49f129cf8be9",
+    "url": "/le-cuenta-un-pajarito/static/js/main.650401e9.chunk.js"
+  },
+  {
+    "revision": "7bd43010801085d5b0b6",
+    "url": "/le-cuenta-un-pajarito/static/js/runtime~main.6be4ba8b.js"
+  },
+  {
+    "revision": "fa6c2931046601031c6fa935c2453b20",
+    "url": "/le-cuenta-un-pajarito/static/media/Final_alpha.fa6c2931.png"
+  },
+  {
+    "revision": "bc62068156157f35f43e8a9b24e8f872",
+    "url": "/le-cuenta-un-pajarito/static/media/Final_happy_alpha_alt.bc620681.png"
+  },
+  {
+    "revision": "f5c4583d4c43eb8a98b1189f9c347e3e",
+    "url": "/le-cuenta-un-pajarito/static/media/Piolitica.f5c4583d.png"
+  }
+]);
