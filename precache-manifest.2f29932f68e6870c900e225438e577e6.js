028fc2ead2473736b2b5c0879051c6a16686e94f
Leo <leodesouzauy@hotmail.com>
2019-12-31 15:47:48 -0300

    Updates

--- precache-manifest.2f29932f68e6870c900e225438e577e6.js
+++ precache-manifest.2f29932f68e6870c900e225438e577e6.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cf43f43aadb06f4b345304c510d692b9",
+    "revision": "5f0f445af22ca9a4725a30a15bd6f4d7",
     "url": "/le-cuenta-un-pajarito/index.html"
   },
   {
-    "revision": "1d3727af77c22138b8f5",
-    "url": "/le-cuenta-un-pajarito/static/css/main.e7c0c871.chunk.css"
+    "revision": "1bf54dbc9ec42c387d36",
+    "url": "/le-cuenta-un-pajarito/static/css/main.a156c6cc.chunk.css"
   },
   {
     "revision": "d35d344bc543654f2b0d",
     "url": "/le-cuenta-un-pajarito/static/js/2.9e897f10.chunk.js"
   },
   {
-    "revision": "1d3727af77c22138b8f5",
-    "url": "/le-cuenta-un-pajarito/static/js/main.0120fa38.chunk.js"
+    "revision": "1bf54dbc9ec42c387d36",
+    "url": "/le-cuenta-un-pajarito/static/js/main.021a1e5d.chunk.js"
   },
   {
     "revision": "7bd43010801085d5b0b6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/le-cuenta-un-pajarito/precache-manifest.2781b3420aaee04bcdfbaddfb62dba4a.js"
+  "/le-cuenta-un-pajarito/precache-manifest.2f29932f68e6870c900e225438e577e6.js"
 );
 
 self.addEventListener('message', (event) => {
